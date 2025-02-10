ceb4aa4a3c3b36a14e52569c0279cb19b61ef1d1
George Ikigu <ikigu.geo@gmail.com>
2025-02-10 22:31:14 +0300

    feat: add ability to create an empty summary workbook

---
+++ summary.py
@@ -0,0 +1,105 @@
+# Check if collection summary file exists within COLLECTION SUMMARY folder in parent folder
+
+import calendar
+from datetime import datetime
+import os
+from openpyxl import Workbook
+
+
+def get_days_in_month(year: int, month: int) -> int:
+    return calendar.monthrange(year, month)[1]
+
+
+def month_str_to_int(month_name: str) -> int:
+    return datetime.strptime(month_name, "%B").month
+
+
+def get_year_from_file_name(file_name: str) -> int:
+    file_name = os.path.basename(file_name)
+    year = file_name.split(" ")[2].split(".")[0]
+
+    return int(year)
+
+
+def get_month_from_file_name(file_name: str) -> str:
+    file_name = os.path.basename(file_name)
+    month = file_name.split(" ")[1]
+
+    return month
+
+
+def create_summary_file_path(shift_change_file_path):
+    """
+    Gets the path for the summary file, given a shift change file path
+
+    Args:
+        shift_change_file_path (str): The path of the shift change excel file
+    """
+    month = get_month_from_file_name(shift_change_file_path)
+    year = get_year_from_file_name(shift_change_file_path)
+    parent_directory = os.path.dirname(shift_change_file_path)
+    summary_file_path = os.path.join(
+        parent_directory, "COLLECTION SUMMARY", f"{month} {year}.xlsx")
+
+    return summary_file_path
+
+
+def create_summary_file(shift_change_file_path, summary_workbook_map):
+    """
+    Creates the summary Excel workbook, adds the sheets required
+    and creates the tables in each sheet
+    """
+    summary_workbook = Workbook()
+    summary_file_path = create_summary_file_path(shift_change_file_path)
+
+    # Retrieve summary workbook metadata
+    sheet_names = summary_workbook_map["sheets"]["names"]
+    table_title_cell = summary_workbook_map["table"]["title_cell"]
+    table_column_titles = summary_workbook_map["table"]["column_titles"]
+    date_column = summary_workbook_map["table"]["date_column"]
+    first_date_row = summary_workbook_map["table"]["first_date_row"]
+
+    # Get date metadata
+    month = month_str_to_int(get_month_from_file_name(shift_change_file_path))
+    year = get_year_from_file_name(shift_change_file_path)
+    number_of_days = get_days_in_month(year, month)
+
+    # Create sheets
+    for sheet_name in sheet_names:
+        summary_workbook.create_sheet(sheet_name)
+
+    summary_workbook.remove(summary_workbook["Sheet"])
+
+    # Create tables in sheets
+    for sheet_name in sheet_names:
+        sheet = summary_workbook[sheet_name]
+        sheet[table_title_cell] = sheet_name
+
+        # Add table titles
+        for cell, title in table_column_titles.items():
+            sheet[cell] = title
+
+        # Add dates
+        for day in range(1, number_of_days + 1):
+            cell = f"{date_column}{(first_date_row - 1) + day}"
+            sheet[cell] = day
+
+    # Create COLLECTION SUMMARY directory
+    try:
+        os.makedirs(os.path.dirname(summary_file_path,), exist_ok=False)
+    except FileExistsError:
+        print("Error: A COLLECTION SUMMARY folder already exists in this directory!")
+        exit(1)
+
+    # Save workbook
+    summary_workbook.save(summary_file_path)
+    summary_workbook.close()
+
+# TODO: Add formulas to calculate totals at bottom
+# TODO: Lengthen the columns
+# TODO: Populate the data
+
+
+# Open the previous day's excel file, passed in as an argument to the program
+# Open the month's summary file
+# Copy the values you need
