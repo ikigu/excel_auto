d283034db06f9743b9a0b7b0467dfab339e3d4bf
George Ikigu <ikigu.geo@gmail.com>
2025-02-14 13:24:58 +0300

    refactor: remove logic for file path creation

--- summary.py
+++ summary.py
@@ -1,55 +1,9 @@
 # Check if collection summary file exists within COLLECTION SUMMARY folder in parent folder
 
-import calendar
-from datetime import datetime
 import os
+from datetime import datetime
+from filename import create_summary_file_path, month_str_to_int, get_year_from_file_name, get_month_from_file_name, get_days_in_month
 from openpyxl import Workbook
-
-
-def get_days_in_month(year: int, month: int) -> int:
-    return calendar.monthrange(year, month)[1]
-
-
-def month_str_to_int(month_name: str) -> int:
-    return datetime.strptime(month_name, "%B").month
-
-
-def get_year_from_file_name(file_name: str) -> int:
-    file_name = os.path.basename(file_name)
-    year = file_name.split(" ")[2].split(".")[0]
-
-    return int(year)
-
-
-def get_month_from_file_name(file_name: str) -> str:
-    file_name = os.path.basename(file_name)
-    month = file_name.split(" ")[1]
-
-    return month
-
-
-def get_day_from_file_name(file_name: str) -> int:
-    file_name = os.path.basename(file_name)
-    day_with_suffix = file_name.split(" ")[0]
-    day_without_suffix = day_with_suffix[:-2]
-
-    return int(day_without_suffix)
-
-
-def create_summary_file_path(shift_change_file_path):
-    """
-    Gets the path for the summary file, given a shift change file path
-
-    Args:
-        shift_change_file_path (str): The path of the shift change excel file
-    """
-    month = get_month_from_file_name(shift_change_file_path)
-    year = get_year_from_file_name(shift_change_file_path)
-    parent_directory = os.path.dirname(shift_change_file_path)
-    summary_file_path = os.path.join(
-        parent_directory, "COLLECTION SUMMARY", f"{month} {year}.xlsx")
-
-    return summary_file_path
 
 
 def create_summary_file(shift_change_file_path, summary_workbook_map):
