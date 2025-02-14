0bb437a34e5781870b6490b7d8a3cfb2d05b54da
George Ikigu <ikigu.geo@gmail.com>
2025-02-14 13:23:44 +0300

    refactor: move all logic for creating paths to filename.py

--- filename.py
+++ filename.py
@@ -1,5 +1,8 @@
+import calendar
 import os
 from datetime import datetime
+from pathlib import Path
+from datetime import datetime, timedelta
 
 
 def get_day_with_suffix(day):
@@ -17,33 +20,47 @@ def get_day_with_suffix(day):
     return f"{day}{suffix}"
 
 
-def create_new_file_path(old_file_path):
+def get_days_in_month(year: int, month: int) -> int:
+    return calendar.monthrange(year, month)[1]
+
+
+def month_str_to_int(month_name: str) -> int:
+    return datetime.strptime(month_name, "%B").month
+
+
+def get_year_from_file_name(file_name: str) -> int:
+    file_name = os.path.basename(file_name)
+    year = file_name.split(" ")[2].split(".")[0]
+
+    return int(year)
+
+
+def get_month_from_file_name(file_name: str) -> str:
+    file_name = os.path.basename(file_name)
+    month = file_name.split(" ")[1]
+
+    return month
+
+
+def get_day_from_file_name(file_name: str) -> int:
+    file_name = os.path.basename(file_name)
+    day_with_suffix = file_name.split(" ")[0]
+    day_without_suffix = day_with_suffix[:-2]
+
+    return int(day_without_suffix)
+
+
+def create_summary_file_path(shift_change_file_path):
     """
-    Creates the a path with the new file name according to today's date
+    Gets the path for the summary file, given a shift change file path
 
     Args:
-        old_file_path (str): The path to the existing .xlsx file.
-
-    Returns:
-        str: The new file path if successful.
-
-    Raises:
-        FileNotFoundError: If the old file path does not exist.
-        ValueError: If the new file name does not have the .xlsx extension.
+        shift_change_file_path (str): The path of the shift change excel file
     """
-    if not os.path.isfile(old_file_path):
-        raise FileNotFoundError(f"The file '{old_file_path}' does not exist.")
+    month = get_month_from_file_name(shift_change_file_path)
+    year = get_year_from_file_name(shift_change_file_path)
+    parent_directory = os.path.dirname(shift_change_file_path)
+    summary_file_path = os.path.join(
+        parent_directory, "COLLECTION SUMMARY", f"{month} {year}.xlsx")
 
-    # Extract the directory of the old file
-    directory = os.path.dirname(old_file_path)
-
-    # Get today's date
-    date = datetime.now()
-
-    # Create the new file name with today's date
-    new_file_name = f"{get_day_with_suffix(date.day)} {date.strftime('%B %Y')}.xlsx"
-
-    # Construct the new file path
-    new_file_path = os.path.join(directory, new_file_name)
-
-    return new_file_path
+    return summary_file_path
