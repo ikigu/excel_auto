3f30f8ea9a91d3f0a0c1babfb0e0b2e6f7bbb591
George Ikigu <ikigu.geo@gmail.com>
2025-06-17 06:32:51 +0300

    fix: correct copying algo

--- summary.py
+++ summary.py
@@ -17,7 +17,7 @@ def create_summary_file(shift_change_file_path, summary_workbook_map):
     # Retrieve summary workbook metadata
     sheet_names = summary_workbook_map["sheets"]["names"]
     table_title_cell = summary_workbook_map["table"]["title_cell"]
-    table_column_titles = summary_workbook_map["table"]["column_titles"]
+    table_column_titles = {}
     date_column = summary_workbook_map["table"]["date_column"]
     first_date_row = summary_workbook_map["table"]["first_date_row"]
 
@@ -33,7 +33,7 @@ def create_summary_file(shift_change_file_path, summary_workbook_map):
         # Change column width
         for column in range(ord("C"), ord("J") + 1):
             summary_workbook[sheet_name].column_dimensions[chr(
-                column)].width = 22
+                column)].width = 24
 
     summary_workbook.remove(summary_workbook["Sheet"])
 
@@ -43,7 +43,20 @@ def create_summary_file(shift_change_file_path, summary_workbook_map):
         sheet[table_title_cell] = sheet_name
 
         # Add table titles
+        if sheet_name == "CASH":
+            table_column_titles = summary_workbook_map["table"]["column_titles"]["cash"]
+        else:
+            table_column_titles = summary_workbook_map["table"]["column_titles"]["default"]
+
         for cell, title in table_column_titles.items():
+            if "COLLECTION" in title:
+                if len(sheet_name.split(" ")) > 1:
+                    sheet_name_list = sheet_name.split(" ")
+                    first_word = sheet_name_list[0]
+                    second_word = sheet_name_list[1]
+                    title = (" ").join([first_word, second_word, "COLLECTION"])
+                else:
+                    title = sheet_name + " " + "COLLECTION"
             sheet[cell] = title
 
         # Add dates
@@ -51,10 +64,25 @@ def create_summary_file(shift_change_file_path, summary_workbook_map):
             cell = f"{date_column}{(first_date_row - 1) + day}"
             sheet[cell] = day
 
+        totals_cell = f"{date_column}{first_date_row + number_of_days}"
+        sheet[totals_cell].value = "TOTALS"
+
         # Add total formulas
-        for column in range(ord("C"), ord("J") + 1):
+        end_column = "J" if sheet_name == "CASH" else "G"
+
+        for column in range(ord("C"), ord(end_column) + 1):
             formula_cell = f"{chr(column)}{first_date_row + number_of_days}"
             sheet[formula_cell].value = f"=SUM({chr(column)}{first_date_row}:{chr(column)}{(number_of_days + first_date_row) - 1})"
+
+        for row in range(first_date_row, (number_of_days + first_date_row)):
+            formula_cell = f"{end_column}{row}"
+
+            if end_column == "J":
+                formula = f"=(H{row} - I{row})"
+            else:
+                formula = f"=(E{row} - F{row})"
+
+            sheet[formula_cell].value = formula
 
     # Create COLLECTION SUMMARY directory
     try:
@@ -67,8 +95,6 @@ def create_summary_file(shift_change_file_path, summary_workbook_map):
     summary_workbook.save(summary_file_path)
     summary_workbook.close()
 
-# TODO: Add formulas to calculate totals at bottom
-
 
 def transfer_data(shift_change_file: object, summary_file: object, summary_map: dict,  day_number: int,):
     source_sheet = shift_change_file[summary_map["data_transfer"]
@@ -76,10 +102,17 @@ def transfer_data(shift_change_file: object, summary_file: object, summary_map: 
     first_date_row = summary_map["table"]["first_date_row"]
     day = str(day_number + (first_date_row - 1))
 
+    transfer_map = {}
+
     for summary_sheet_name, source_column in summary_map["data_transfer"]["source_columns"].items():
         destination_sheet = summary_file[summary_sheet_name]
 
-        for source_row, destination_column in summary_map["data_transfer"]["map"].items():
+        if summary_sheet_name == "CASH":
+            transfer_map = summary_map["data_transfer"]["map"]["cash"]
+        else:
+            transfer_map = summary_map["data_transfer"]["map"]["default"]
+
+        for source_row, destination_column in transfer_map.items():
             source_cell = source_sheet[f"{source_column}{source_row}"]
             destination_cell = destination_sheet[
                 f"{destination_column}{day}"]
